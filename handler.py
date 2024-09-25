8f7a40d5f392559a7581769608095219636da384
Satendra Rai <satendrarai5@gmail.com>
2024-09-25 19:12:11 +0530

    ydl changes

--- handler.py
+++ handler.py
@@ -2,27 +2,26 @@ import sieve
 from typing import Literal
 
 metadata = sieve.Metadata(
-    description="Download highest-resolution version of YouTube video as an MP4.",
+    title = "YouTube Video Downloader",
+    description="Download YouTube videos in MP4 format at any resolution.",
     code_url="https://github.com/sieve-community/examples/blob/main/youtube_downloader",
     image=sieve.Image(
         url="https://yt3.googleusercontent.com/584JjRp5QMuKbyduM_2k5RlXFqHJtQ0qLIPZpwbUjMJmgzZngHcam5JMuZQxyzGMV5ljwJRl0Q=s900-c-k-c0x00ffffff-no-rj"
     ),
     tags=["Video"],
-   # readme=open("README.md", "r").read(),
+    # readme=open("README.md", "r").read(),
 )
-
 
 @sieve.function(
     name="youtube_info",
-    # system_packages=["ffmpeg"],
-    python_packages=[
-        "pytube @ git+https://github.com/sieve-community/pytube.git",
-    ],
-    metadata=metadata
+    system_packages=["ffmpeg"],
+    python_packages=["yt-dlp", "pytube"],
+    metadata=metadata,
 )
 def download(
     url: str,
     resolution: Literal["highest-available", "lowest-available", "1080p", "720p", "480p", "360p", "240p", "144p"] = "1080p",
+    include_audio: bool = True,
 ):
     '''
     :param url: YouTube URL to download
@@ -30,52 +29,47 @@ def download(
     :param include_audio: Whether to include audio in the video.
     :return: The downloaded YouTube video
     '''
-    from pytube import YouTube
-    import time
+    import yt_dlp
+    import os
 
-    print("setting stream...")
-    data = {}
+    # output_filename = "output.mp4"
 
-    #print("filtering stream for highest quality mp4...")
-    stream_tries = 10
-    while stream_tries > 0:
-        try:
-            yt = YouTube(url)
-            data["title"] = yt.title
-            data["duration"] = yt.length
-            if yt.description:
-                data["description"] = yt.description
-            if yt.thumbnail_url:
-                data["poster_url"] = yt.thumbnail_url
+    # if os.path.exists(output_filename):
+    #     print("Deleting existing output file...")
+    #     os.remove(output_filename)
 
-            all_streams = yt.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc() #.first()
-            video = [stream for stream in all_streams if stream.video_codec.startswith('avc1')]
-            break
-        except Exception as e:
-            print(f"Error filtering stream: {e}")
-            stream_tries -= 1
-            time.sleep(0.3)
-            if stream_tries == 0:
-                raise e
-            
-    if resolution == "highest-available":
-        video = video[0]
-        print(f"highest available resolution is {video.resolution}...")
-    elif resolution == "lowest-available":
-        video = video[-1]
-        print(f"lowest available resolution is {video.resolution}...")
-    else:
-        desired_res = int(resolution.replace('p', ''))
-        diff_list = [(abs(desired_res - int(stream.resolution.replace('p', ''))), stream) for stream in video]
-        diff_list.sort(key=lambda x: x[0])
-        video = diff_list[0][1]
-        if video.resolution != resolution:
-            print(f"{resolution} resolution is not available, using {video.resolution} instead...")
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4][vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4][vcodec^=avc1]/best',
+        # 'outtmpl': output_filename,
+        'quiet': True,  # Suppress output
+        'no_warnings': True,  # Suppress warnings
+        'noprogress': True,  # Disable progress bar
+    }
+
+    if resolution != "highest-available":
+        if resolution == "lowest-available":
+            ydl_opts['format'] = 'worstvideo[ext=mp4][vcodec^=avc1]+worstaudio[ext=m4a]/worst[ext=mp4][vcodec^=avc1]/worst'
         else:
-            print(f"selected resolution is {resolution}...")
+            ydl_opts['format'] = f'bestvideo[height<={resolution[:-1]}][ext=mp4][vcodec^=avc1]+bestaudio[ext=m4a]/best[height<={resolution[:-1]}][ext=mp4][vcodec^=avc1]/best'
 
-        
-    print('getting file size...')
-    data["file_size"] = video.filesize
+    if not include_audio:
+        ydl_opts['format'] = ydl_opts['format'].split('+')[0]
 
+    print("Downloading video...")
+    data = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        data["title"] = info_dict.get('title', None) 
+        data["duration"] = info_dict.get('duration', None)
+        data["description"] = info_dict.get('description', None)
+        data["poster_url"] = info_dict.get('thumbnail', None)
+        # data["poster_url"] = info_dict.get('thumbnails', [{}])[0].get('url', None)
+        data["file_size"] = info_dict.get('requested_formats', [{}])[0].get('filesize', None)
+
+
+    print('Done!')
     return data
+    # return sieve.File(path=output_filename)
+
+# if __name__ == "__main__":
+#    download("https://www.youtube.com/watch?v=AKJfakEsgy0")
